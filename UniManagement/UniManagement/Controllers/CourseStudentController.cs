765dae68fd0518ef51c43d710c51b74918cef73e
RISHAD-prog <20-43002-1@student.aiub.edu>
2022-11-30 22:03:38 +0600

    UMS

---
+++ UniManagement/UniManagement/Controllers/CourseStudentController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniManagement.Db;
+
+namespace UniManagement.Controllers
+{
+    public class CourseStudentController : Controller
+    {
+        // GET: CourseStudent
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult Student()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Student(Student students)
+        {
+            UniversityEntities uni = new UniversityEntities();
+             
+            uni.Students.Add(students);
+            uni.SaveChanges();
+           
+            return RedirectToAction("Index");
+           
+        }
+        [HttpGet]
+        public ActionResult Courses()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Courses(Cours courses)
+        {
+            UniversityEntities uni = new UniversityEntities();
+
+            uni.Courses.Add(courses);
+            uni.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ UniManagement/UniManagement/Controllers/RegistrationController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniManagement.Db;
+
+namespace UniManagement.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult PreRegistration()
+        {
+            UniversityEntities uni = new UniversityEntities();
+            var course=uni.Courses.ToList();
+            
+            return View(course);
+        }
+        [HttpPost]  
+        public ActionResult PreRegistration(int[] course)
+        {
+            UniversityEntities university = new UniversityEntities();
+            foreach (int courseId in course)
+            { 
+                university.CourseStudents.Add(new CourseStudent()
+                {
+                    StudentId= 3,
+                    CourseId= courseId,
+                    Status="Incomplete",
+                    Grade="C",
+                    Marks=70
+                });
+
+            }
+            university.SaveChanges();
+            return RedirectToAction("PreRegistration", "Registration");
+        }
+    }
+}
